db8243ca201081a4850c34be9f1253e389583c11
Hoshyar Iranpour <276282555+alirezairanpoor@users.noreply.github.com>
2026-05-15 08:03:21 +0200

    Format project with Prettier

--- server.js
+++ server.js
@@ -1,50 +1,49 @@
-import express from 'express'
-import http from 'http'
-import { Server } from 'socket.io'
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
 
-const app = express()
-const server = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
     origin: '*',
-    methods: ['GET', 'POST']
+    methods: ['GET', 'POST'],
   },
   transports: ['websocket'],
   pingTimeout: 60000,
-  pingInterval: 25000
-})
+  pingInterval: 25000,
+});
 
-let sharedState = {}
+let sharedState = {};
 
 io.engine.on('connection_error', (err) => {
   console.log('ENGINE CONNECTION ERROR:', {
     code: err.code,
     message: err.message,
-    context: err.context
-  })
-})
-
+    context: err.context,
+  });
+});
 
 io.on('connection', (socket) => {
-  console.log('Client connected:', socket.id)
+  console.log('Client connected:', socket.id);
 
-  socket.emit('state-sync', sharedState)
+  socket.emit('state-sync', sharedState);
 
   socket.on('update-state', (newState) => {
     sharedState = {
       ...sharedState,
-      ...newState
-    }
+      ...newState,
+    };
 
-    io.emit('state-sync', sharedState)
-  })
+    io.emit('state-sync', sharedState);
+  });
 
   socket.on('disconnect', () => {
-    console.log('Client disconnected:', socket.id)
-  })
-})
+    console.log('Client disconnected:', socket.id);
+  });
+});
 
 server.listen(3001, '0.0.0.0', () => {
-  console.log('WebSocket server running on port 3001')
-})
+  console.log('WebSocket server running on port 3001');
+});
